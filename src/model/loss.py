16fa2d7c1b69d758f58d6b7caa0991fd8ff6dbf7
Matthew <koltunov.matwei@gmail.com>
2025-03-22 23:50:04 +0500

    refactored code

--- src/model/loss.py
+++ src/model/loss.py
@@ -70,8 +70,8 @@ class SupConLoss(nn.Module):
             self.emo_anchor = torch.load(f"{args.anchor_path}/meld_emo.pt")
             self.emo_label = torch.tensor([0, 1, 2, 3, 4, 5, 6])
         elif args.dataset_name == "IEMOCAP4":
-            self.emo_anchor = torch.load(f"{args.anchor_path}/iemocap_emo.pt").to(self.device)
-            self.emo_label = torch.tensor([0, 1, 2, 3]).to(self.device)
+            self.emo_anchor = torch.load(f"{args.anchor_path}/iemocap_emo.pt")
+            self.emo_label = torch.tensor([0, 1, 2, 3])
         # elif args.dataset_name == "EmoryNLP":
         #     self.emo_anchor = torch.load(f"{args.anchor_path}/emorynlp_emo.pt")
         #     self.emo_label = torch.tensor([0, 1, 2, 3, 4, 5, 6])
